d80560b166a1d28cf53efc610ba08a223bfc9e57
Srishti Ramanathan <srishtiramanathan@gmail.com>
2024-10-11 16:08:43 +0200

    ggplot

--- 03_R Script.R
+++ 03_R Script.R
@@ -64,3 +64,90 @@ gapminder %>%
              colour = country)) +
   geom_line()
 
+#using facet wrap
+
+gapminder %>% 
+  filter(continent == "Americas") %>% 
+  ggplot(aes(x = year, 
+             y = lifeExp,
+             group = country,
+             colour = country)) +
+  geom_line() +
+  facet_wrap(~country)
+
+#giving titles
+
+gapminder %>% 
+  filter(continent == "Americas") %>% 
+  ggplot(aes(x = year, 
+             y = lifeExp,
+             group = country,
+             colour = country)) +
+  geom_line(show.legend = FALSE) +
+  facet_wrap(~country) +
+  labs(x = "Year",
+       y = "Life Expectancy",
+       title = "Figure 1")
+
+#setting a theme
+
+gapminder %>% 
+  filter(continent == "Americas") %>% 
+  ggplot(aes(x = year, 
+             y = lifeExp,
+             group = country,
+             colour = country)) +
+  geom_line(show.legend = FALSE) + #to remove legends if redundant
+  facet_wrap(~country) +
+  labs(x = "Year",
+       y = "Life Expectancy",
+       title = "Figure 1") +
+  theme(axis.text.x = element_text(angle = 90,
+                                   hjust = 1))
+
+
+R_plot <- gapminder %>% 
+  filter(continent == "Americas") %>% 
+  ggplot(aes(x = year, 
+             y = lifeExp,
+             group = country,
+             colour = country)) +
+  geom_line(show.legend = FALSE) +
+  facet_wrap(~country) +
+  labs(x = "Year",
+       y = "Life Expectancy",
+       title = "Figure 1") +
+  theme(axis.text.x = element_text(angle = 90,
+                                   hjust = 1))
+                                   
+#saving the plots
+#you can use the GUI to save it but also code.
+
+ggsave(filename = "Result.png", plot = R_plot,
+       width = 15,
+       height = 20,
+       dpi = 500,
+       units = "cm")
+
+?geom_boxplot
+
+#Generate box plots to compare life expectancy between the different continents during the available years. 
+#(Specifically: x axis as continent, y axis as lifeExp, and fill the boxes using different colors for continents. Facet using year.)
+#💡   Hint: Use “fill” in aes function to generate solid boxes.
+
+Plot_2 <- gapminder %>% 
+  ggplot(aes(x = continent, 
+             y = lifeExp,
+             fill = continent
+               )) +
+  geom_boxplot(show.legend = FALSE) +
+  facet_wrap(~year)
+  labs(x = "Continent",
+       y = "Life Expectancy",
+       title = "Figure 1")
+  
+  Plot_2
+  ggsave(filename = "Result2.png", plot = Plot_2,
+         width = 20,
+         dpi = 500)
+  
